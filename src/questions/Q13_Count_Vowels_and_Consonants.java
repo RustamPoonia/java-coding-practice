04175ba52e648c662679b2b9436614d7915490ae
Rustam <rustampoonia@gmail.com>
2025-07-18 21:45:59 +0530

    Day 3/10 of practising questions

--- src/questions/Q13_Count_Vowels_and_Consonants.java
+++ src/questions/Q13_Count_Vowels_and_Consonants.java
@@ -2,6 +2,22 @@ package questions;
 
 public class Q13_Count_Vowels_and_Consonants {
     public static void main(String[] args) {
+        String str="Ramu is a good boy";
 
+        char[] charArr=str.toLowerCase().toCharArray();
+
+        int vowels = 0;
+        int consonants =0;
+
+        for(int i=0;i<str.length();i++){
+           char ch =str.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                vowels ++;
+            }
+            else {
+                consonants++;
+            }
+        }
+        System.out.println("Vowels :"+vowels+" Consonants :"+consonants);
     }
 }
